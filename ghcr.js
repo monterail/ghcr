af2bcf8e06b5309a830317bfc7751fbb8dd01736
Tymon Tobolski <i@teamon.eu>
2013-05-20 00:04:15 +0200

    Pending commits

--- ghcr.js
+++ ghcr.js
@@ -20,14 +20,60 @@
         return $.post("" + url + "/save", $.extend({}, data, {
           repo: repo
         }), cb);
+      },
+      pending: function(user, cb) {
+        return $.get("" + url + "/pending", {
+          repo: repo,
+          user: user
+        }, cb, 'json');
+      },
+      pendingCount: function(user, cb) {
+        return $.get("" + url + "/pending/count", {
+          repo: repo,
+          user: user
+        }, cb, 'json');
       }
     };
   };
 
   GHCR = {
     init: function(apiUrl, repo) {
+      this.repo = repo;
       this.api = API(apiUrl, repo);
-      return this.user = $.trim($("#user-links .name").text());
+      this.user = $.trim($("#user-links .name").text());
+      return this.initPendingTab();
+    },
+    initPendingTab: function() {
+      var _this = this;
+      return this.api.pendingCount(this.user, function(res) {
+        var $a, $li, $ul;
+        $("a#ghcr-pending-tab").remove();
+        $ul = $("li a.tabnav-tab:contains('Commits')").parent().parent();
+        $li = $("<li/>");
+        $a = $("<a href='#ghcr-pending' id='ghcr-pending-tab' class='tabnav-tab'>Pending commits <span class='counter'>" + res.count + "</span></a>").click(function() {
+          return _this.pending();
+        });
+        $li.append($a);
+        return $ul.append($li);
+      });
+    },
+    pending: function() {
+      var _this = this;
+      return this.api.pending(this.user, function(commits) {
+        var $container, $ol, commit, diffUrl, treeUrl, _i, _len;
+        $(".tabnav-tabs a").removeClass("selected");
+        $("#ghcr-pending-tab").addClass("selected");
+        $container = $("#js-repo-pjax-container");
+        $container.html("<h3 class=\"commit-group-heading\">Pending commits</h3>");
+        $ol = $("<ol class='commit-group'/>");
+        for (_i = 0, _len = commits.length; _i < _len; _i++) {
+          commit = commits[_i];
+          diffUrl = "/" + _this.repo + "/commit/" + commit.id;
+          treeUrl = "/" + _this.repo + "/tree/" + commit.id;
+          $ol.append($("<li class=\"commit commit-group-item js-navigation-item js-details-container\">\n  <p class=\"commit-title  js-pjax-commit-title\">\n    <a href=\"" + diffUrl + "\" class=\"message\">" + commit.message + "</a>\n  </p>\n  <div class=\"commit-meta\">\n    <div class=\"commit-links\">\n      <span class=\"js-zeroclipboard zeroclipboard-button\" data-clipboard-text=\"" + commit.id + "\" data-copied-hint=\"copied!\" title=\"Copy SHA\">\n        <span class=\"octicon octicon-clippy\"></span>\n      </span>\n\n      <a href=\"" + diffUrl + "\" class=\"gobutton \">\n        <span class=\"sha\">" + (commit.id.substring(0, 10)) + "\n          <span class=\"octicon octicon-arrow-small-right\"></span>\n        </span>\n      </a>\n\n      <a href=\"" + treeUrl + "\" class=\"browse-button\" title=\"Browse the code at this point in the history\" rel=\"nofollow\">\n        Browse code <span class=\"octicon octicon-arrow-right\"></span>\n      </a>\n    </div>\n\n    <div class=\"authorship\">\n      <span class=\"author-name\"><a href=\"/" + commit.author.username + "\" rel=\"author\">" + commit.author.username + "</a></span>\n      authored <time class=\"js-relative-date\" datetime=\"" + commit.timestamp + "\" title=\"2013-03-17 16:56:15\">2 days before the day after tomorow</time>\n    </div>\n  </div>\n</li>"));
+        }
+        return $container.append($ol);
+      });
     },
     commitsPage: function() {
       var e, ids;
@@ -53,12 +99,11 @@
         return _results;
       });
     },
-    commitPage: function() {
-      var id, render,
+    commitPage: function(id) {
+      var render,
         _this = this;
-      id = $(".full-commit .sha.js-selectable-text").text();
       render = function(commit) {
-        var $box, $btn, btnlbl, fun, info;
+        var $box, $btn, btnlbl, info, status;
         if (commit == null) {
           commit = {};
         }
@@ -66,27 +111,24 @@
         commit.id || (commit.id = id);
         switch (commit.status) {
           case "accepted":
-            fun = function() {
-              commit.status = "pending";
-              commit.user = _this.user;
-              _this.api.save(commit);
-              return render(commit);
-            };
+            status = "pending";
             btnlbl = "Make pending";
             console.log(commit.created_at);
-            info = "Commit accepted by <a href='https://github.com/" + commit.user + "'>" + commit.user + "<a/> at " + commit.created_at;
+            info = "Commit accepted by <a href='https://github.com/" + commit.reviewer + "'>" + commit.reviewer + "<a/> at " + commit.updated_at;
             break;
           default:
-            fun = function() {
-              commit.status = "accepted";
-              commit.user = _this.user;
-              _this.api.save(commit);
-              return render(commit);
-            };
+            status = "accepted";
             btnlbl = "Accept";
             info = "Code review pending";
         }
-        $btn = $("<button class='minibutton'>" + btnlbl + "</button>").click(fun);
+        $btn = $("<button class='minibutton'>" + btnlbl + "</button>").click(function() {
+          commit.status = status;
+          commit.reviewer = _this.user;
+          return _this.api.save(commit, function(data) {
+            render(data);
+            return _this.initPendingTab();
+          });
+        });
         $("#ghcr-box").remove();
         $box = $("<div id='ghcr-box' class='ghcr-" + commit.status + "'><span>" + info + "</span> </div>");
         $box.append($btn);
@@ -102,11 +144,15 @@
     repo = "" + chunks[1] + "/" + chunks[2];
     apiUrl = 'http://localhost:9393/ghcr';
     GHCR.init(apiUrl, repo);
-    switch (chunks[3]) {
-      case "commits":
-        return GHCR.commitsPage();
-      case "commit":
-        return GHCR.commitPage();
+    if (window.location.hash === "#ghcr-pending") {
+      return GHCR.pending();
+    } else {
+      switch (chunks[3]) {
+        case "commits":
+          return GHCR.commitsPage();
+        case "commit":
+          return GHCR.commitPage(chunks[4]);
+      }
     }
   });
 
